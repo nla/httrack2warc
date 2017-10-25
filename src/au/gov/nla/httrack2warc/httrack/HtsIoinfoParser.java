2132905306a6cd589f6336e085bc03fa0f537b7b
Alex Osborne <aosborne@nla.gov.au>
2017-10-25 18:51:46 +0900

    Cope with newer ioinfo log versions that have a numeric prefix

--- src/au/gov/nla/httrack2warc/httrack/HtsIoinfoParser.java
+++ src/au/gov/nla/httrack2warc/httrack/HtsIoinfoParser.java
@@ -8,7 +8,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class HtsIoinfoParser implements Closeable {
-    private static final Pattern HEADER_RE = Pattern.compile("(request|response) for (.*):");
+    private static final Pattern HEADER_RE = Pattern.compile("(?:\\[\\d+\\] )?(request|response) for (.*):");
     private final BufferedReader reader;
     boolean request;
     String filename;
